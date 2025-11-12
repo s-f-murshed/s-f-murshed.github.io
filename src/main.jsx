ff521467579d4ee2ac0a34a5abd38128d4e065c7
s-f-murshed <farhanmurshed786@gmail.com>
2025-11-12 20:53:11 +0530

    Rebuild

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="*" element={<App />} /> {/* Handles unknown routes */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
